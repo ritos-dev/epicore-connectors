154c0689710adf1941e3cf2a8058cb5639c03163
MaciejTB <maciej@risskovteknik.dk>
2025-10-20 18:52:32 +0200

    Ok

--- RTS.SharedKernel/Interfaces/IDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace RTS.SharedKernel.Interfaces
-{
-    public interface IDomainEvent : INotification
-    {
-        DateTime DateOccurred { get; }
-    }
-}
--- RTS.SharedKernel/Interfaces/IHandleEvent.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-
-namespace RTS.SharedKernel.Interfaces
-{
-    public interface IHandleEvent<in TEvent> : INotificationHandler<TEvent> where TEvent : IDomainEvent
-    {
-    }
-}
